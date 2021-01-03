14443a1fe6dae534da1c69c2393408727ac5694f
Adi <adikikin88@gmail.com>
2021-01-03 11:19:33 +0200

    model functions declarations

---
+++ src/model.py
@@ -0,0 +1,11 @@
+class Model:
+    def __init__(self):
+        pass
+        # self.state_factory = State_factory()
+        # self.state = self.state_factory.create_state()
+
+    def operate(self, item):
+        pass
+
+    def change_state(self, state):
+        pass
