80a99208d269e3146521da251e9f579c0f4af1ac
Adi <adikikin88@gmail.com>
2021-01-03 17:46:36 +0200

    test view

---
+++ tests/test_view.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('/home/adi/Calculator/src')
+
+from view import View
+
+class i_controller:
+    pass
+
+if __name__ == "__main__":
+    i_ctrl_mock = i_controller()
+    view1 = View(i_ctrl_mock)
+    view1.show_calculator()
+
