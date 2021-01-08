585e8389d89446d9c52e30a683276383db576489
Adi <adikikin88@gmail.com>
2021-01-08 21:31:51 +0200

    factory - implementation of Start state instanciation

--- src/state_factory.py
+++ src/state_factory.py
@@ -1,6 +1,20 @@
-class State_factory:
+from i_state_factory import I_state_factory
+from i_model import I_model
+from start import Start
+
+
+class State_factory(I_state_factory):
+    def __init__(self, i_model):
+        self.i_model = i_model
+        self.state_dict = {
+            "Start" : Start(self, self.i_model)
+            #"Error" : Error(self, self.i_model)
+        }
+
+
     def create_state(self):
-        pass
+        return self.state_dict["Start"]
+        
 
     def create_state_from(self, state):
-        pass
+        return self.state_dict[state.get_next_state_name()]
