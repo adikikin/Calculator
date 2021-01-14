cb639c2e006771678ddf9fd8bbb25cbc9fef7556
Adi <adikikin88@gmail.com>
2021-01-14 13:39:46 +0200

    state classes declarations

---
+++ src/storing_operator.py
@@ -0,0 +1,16 @@
+from state import State
+from states_enum import States_enum
+
+class Storing_operator(State):
+    def __init__(self, i_state_factory, i_model):
+        self.i_factory = i_state_factory
+        self.i_model = i_model
+
+
+    def operate(self, item):
+        pass
+
+
+    def get_next_state_name(self):
+        pass
+        # return self._next_state_name
