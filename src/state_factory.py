01bc487daccfe37c1453b961acee88326f891ff9
Adi <adikikin88@gmail.com>
2021-01-14 12:47:58 +0200

    changed to enum and added asserstion in create_state_from

--- src/state_factory.py
+++ src/state_factory.py
@@ -1,20 +1,23 @@
 from i_state_factory import I_state_factory
 from i_model import I_model
 from start import Start
+from states_enum import States_enum
 
 
 class State_factory(I_state_factory):
     def __init__(self, i_model):
         self.i_model = i_model
         self.state_dict = {
-            "Start" : Start(self, self.i_model)
-            #"Error" : Error(self, self.i_model)
+            States_enum.START : Start(self, self.i_model)
+            # States_enum.ERROR : Error(self, self.i_model)
         }
 
 
     def create_state(self):
-        return self.state_dict["Start"]
+        return self.state_dict[States_enum.START]
         
 
     def create_state_from(self, state):
-        return self.state_dict[state.get_next_state_name()]
+        enum_member = state.get_next_state_name()
+        assert enum_member in self.state_dict
+        return self.state_dict[enum_member]
