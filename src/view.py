b37bfd8c3ab80c3ca1734d28914af0d39a808858
Adi <adikikin88@gmail.com>
2021-01-18 15:16:38 +0200

    removed unused import, improved logic of receive input func

--- src/view.py
+++ src/view.py
@@ -1,6 +1,6 @@
 import random
 import copy
-from tkinter import *
+from tkinter import Tk, Text, Frame, LEFT, RIGHT, END, Button
 from i_controller import i_controller
 from buttons_enum import Buttons_Enum
 
@@ -20,13 +20,17 @@ class View:
     def show_calculator(self):
         self.app_window.mainloop()
 
+    def clear_panel(self):
+        self.panel.delete('1.0', END)
+
     def __receive_input(self, usr_input):
-        self.show_on_panel(usr_input)
+        self.show_on_panel(usr_input) #here now for tests only (should be called be controller)
         if usr_input in range(10):
             self.i_controller.add_digit(usr_input)
-        elif usr_input == Buttons_Enum.CLEAR:
+        elif usr_input == Buttons_Enum.CLEAR.value:
+            self.clear_panel() #here now for tests only (should be called be controller)
             self.i_controller.restart()
-        elif usr_input == Buttons_Enum.EQUAL:
+        elif usr_input == Buttons_Enum.EQUAL.value:
             self.i_controller.evaluate()
         else:
             self.i_controller.add_operator(usr_input)
