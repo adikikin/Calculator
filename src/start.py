6062f3ed95ea2fb510fddd4ced17c47aa2378499
Adi <adikikin88@gmail.com>
2021-01-20 18:13:04 +0200

    changed hard coded strings to enum

--- src/start.py
+++ src/start.py
@@ -1,5 +1,7 @@
 from state import State
 from states_enum import States_enum
+from buttons_enum import Buttons_Enum as Btns
+
 
 class Start(State):
     def __init__(self, i_state_factory, i_model):
@@ -9,13 +11,13 @@ class Start(State):
 
     def operate(self, item):
         self.__reset_expression()
-        if item in ["=", "C"]:
+        if item in [Btns.EQUAL.value, Btns.CLEAR.value]:
             State._next_state_name = States_enum.START
             return_val = None               
-        elif item in ["x", "/", "+"]:
+        elif item in [Btns.MUL.value, Btns.DIV.value, Btns.ADD.value]:
             return_val = State._next_state_name = States_enum.ERROR
         else:
-            if item == "-":
+            if item == Btns.SUB.value:
                 State._sign *= -1
             State._next_state_name = States_enum.STORING_DIGITS_FOR_FIRST_OPERAND
             return_val = item
