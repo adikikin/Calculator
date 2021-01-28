d5a56a2d8f32dc30cbabfa2ff8716dc7c059f419
Adi <adikikin88@gmail.com>
2021-01-28 17:29:03 +0200

    implementation of model and controller

--- src/controller.py
+++ src/controller.py
@@ -1,22 +1,30 @@
 from i_controller import i_controller
 from view import View
+from model import Model
+from buttons_enum import Buttons_Enum
 
 class Controller(i_controller):
     def __init__(self):
-        #self.model = Model()
+        self.model = Model()
         self.view = View(self)
 
     def start(self):
         self.view.show_calculator()
 
     def add_digit(self, digit):
-        pass
+        result = self.model.operate(digit)
+        self.view.show_on_panel(result)
 
     def add_operator(self, operator):
         pass
 
     def evaluate(self):
-        pass
+        self.view.clear_panel()
+        result = self.model.operate(Buttons_Enum.EQUAL.value) 
+        if result:
+            self.view.show_on_panel(result)
+
 
     def restart(self):
-        pass
+        assert self.model.operate(Buttons_Enum.CLEAR.value) == None
+        self.view.clear_panel()
--- src/model.py
+++ src/model.py
@@ -4,12 +4,11 @@ from i_model import I_model
 
 class Model(I_model):
     def __init__(self):
-        pass
-        # self.state_factory = State_factory()
-        # self.state = self.state_factory.create_state()
+        self.state_factory = State_factory(self)
+        self.state = self.state_factory.create_state()
 
     def operate(self, item):
-        pass
+        return self.state.operate(item)
 
     def change_state(self, state):
-        pass
+        self.state = state
