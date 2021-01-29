b4030a8f6bb3312b24fa7b18bc7c991a20fa3568
Adi <adikikin88@gmail.com>
2021-01-29 23:36:58 +0200

    added more abstract methods instead of general operate function

--- src/state.py
+++ src/state.py
@@ -1,20 +1,37 @@
 from abc import ABC, abstractmethod
 from i_state_factory import I_state_factory
 from i_model import I_model
-from expression import Expression
 
 class State(ABC):
     @abstractmethod
-    def __init__(self, i_state_factory, i_model):
+    def __init__(self, i_state_factory, i_model, expression):
         pass
 
-    _expression = Expression()
-    _next_state_name = None
+    # @abstractmethod
+    # @classmethod
+    # def create_from(cls, state)
+
 
     @abstractmethod
-    def operate(self, item):
+    def add_digit(self, digit):
+        pass
+
+    @abstractmethod
+    def add_operator(self, operator):
+        pass
+
+    @abstractmethod
+    def evaluate(self):
+        pass
+
+    @abstractmethod
+    def restart(self):
         pass
     
     @abstractmethod
-    def get_next_state_name(self):
+    def get_next_state_enum(self):
+        pass
+
+    @abstractmethod
+    def get_expression(self):
         pass
