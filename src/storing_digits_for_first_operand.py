6e9ec16cde1083cd259c7b4da7f3ae67c0ccc67e
Adi <adikikin88@gmail.com>
2021-01-29 17:24:02 +0200

    implementation

--- src/storing_digits_for_first_operand.py
+++ src/storing_digits_for_first_operand.py
@@ -1,5 +1,6 @@
 from state import State
 from states_enum import States_enum
+from buttons_enum import Buttons_Enum as Btns
 
 class Storing_digits_for_first_operand(State):
     def __init__(self, i_state_factory, i_model):
@@ -8,9 +9,24 @@ class Storing_digits_for_first_operand(State):
 
 
     def operate(self, item):
-        pass
-
+        if item in range(10):
+            self._expression.add_digit_to_first_operand(item)
+            State._next_state_name = States_enum.STORING_DIGITS_FOR_FIRST_OPERAND
+            return_val = item   
+        elif item == Btns.CLEAR.value:
+            self._expression.reset_expression()
+            State._next_state_name = States_enum.START
+            return_val = None
+        elif item == Btns.EQUAL.value:
+            State._next_state_name = States_enum.STORED_OPERAND
+            return_val = self._expression.get_first_operand()
+        else:
+            self._expression.add_operator(item)
+            State._next_state_name = States_enum.STORED_OPERATOR
+            return_val = item
+        new_state = self.i_factory.create_state_from(self)
+        self.i_model.change_state(new_state)
+        return return_val
 
     def get_next_state_name(self):
-        pass
-        # return self._next_state_name
+        return self._next_state_name
