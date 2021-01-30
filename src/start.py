5feb2e541019c41c157897a5016fae9dea539126
Adi <adikikin88@gmail.com>
2021-01-31 00:10:52 +0200

    changed from "self"  members to ABC "State" members

--- src/start.py
+++ src/start.py
@@ -6,28 +6,28 @@ from buttons_enum import Buttons_Enum as Btns
 
 class Start(State):
     def __init__(self, i_state_factory, i_model, expression):
-        self.i_factory = i_state_factory
-        self.i_model = i_model
-        self.expression = expression
-        self.next_state_enum = None
+        State.i_factory = i_state_factory
+        State.i_model = i_model
+        State.expression = expression
+        State.next_state_enum = None
 
 
     def add_digit(self, digit):
-        self.expression.add_digit_to_first_operand(digit)
-        self.next_state_enum = States_enum.STORING_DIGITS_FOR_FIRST_OPERAND
-        self.__update_model_with_new_state()
+        State.expression.add_digit_to_first_operand(digit)
+        State.next_state_enum = States_enum.STORING_DIGITS_FOR_FIRST_OPERAND
+        State.update_model_with_next_state(self, self)
         return digit
 
 
     def add_operator(self, operator):
         if operator == Btns.SUB.value:
-            self.expression.flip_sign()
-            self.next_state_enum = States_enum.STORING_DIGITS_FOR_FIRST_OPERAND
+            State.expression.flip_sign()
+            State.next_state_enum = States_enum.STORING_DIGITS_FOR_FIRST_OPERAND
             return_val = operator
         else:
-            self.next_state_enum = States_enum.ERROR
+            State.next_state_enum = States_enum.ERROR
             return_val = States_enum.ERROR.name
-        self.__update_model_with_new_state()
+        State.update_model_with_next_state(self, self)
         return return_val
 
 
@@ -38,15 +38,3 @@ class Start(State):
     def restart(self):
         return None 
                       
-
-    def get_next_state_enum(self):
-        return self.next_state_enum
-
-
-    def get_expression(self):
-        return self.expression
-
-
-    def __update_model_with_new_state(self):
-        new_state = self.i_factory.create_state_from(self)
-        self.i_model.change_state(new_state)
