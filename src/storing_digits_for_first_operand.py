e874cccfe8be0d35878434e2f1efd3de4d261619
Adi <adikikin88@gmail.com>
2021-01-31 00:12:04 +0200

    new implementation according to the changes in the interface

--- src/storing_digits_for_first_operand.py
+++ src/storing_digits_for_first_operand.py
@@ -3,30 +3,32 @@ from states_enum import States_enum
 from buttons_enum import Buttons_Enum as Btns
 
 class Storing_digits_for_first_operand(State):
-    def __init__(self, i_state_factory, i_model):
-        self.i_factory = i_state_factory
-        self.i_model = i_model
+    def __init__(self, i_state_factory, i_model, expression):
+        State.i_factory = i_state_factory
+        State.i_model = i_model
+        State.expression = expression
+        State.next_state_enum = None
 
 
-    def operate(self, item):
-        if item in range(10):
-            State._expression.add_digit_to_first_operand(item)
-            State._next_state_name = States_enum.STORING_DIGITS_FOR_FIRST_OPERAND
-            return_val = item   
-        elif item == Btns.CLEAR.value:
-            State._expression.reset_expression()
-            State._next_state_name = States_enum.START
-            return_val = None
-        elif item == Btns.EQUAL.value:
-            State._next_state_name = States_enum.STORED_OPERAND
-            return_val = State._expression.get_first_operand()
-        else:
-            State._expression.add_operator(item)
-            State._next_state_name = States_enum.STORED_OPERATOR
-            return_val = item
-        new_state = self.i_factory.create_state_from(self)
-        self.i_model.change_state(new_state)
-        return return_val
+    def add_digit(self, digit):
+        return State.update_expression_and_model(self, self, 
+                                                 State.expression.add_digit_to_first_operand, 
+                                                 States_enum.STORING_DIGITS_FOR_FIRST_OPERAND,
+                                                 digit)
 
-    def get_next_state_name(self):
-        return State._next_state_name
+    def add_operator(self, operator):
+        return State.update_expression_and_model(self, self, 
+                                                 State.expression.add_operator, 
+                                                 States_enum.STORED_OPERATOR,
+                                                 operator)
+        
+    def restart(self):
+        return State.update_expression_and_model(self, self,
+                                                 State.expression.reset_expression, 
+                                                 States_enum.STORED_OPERATOR)
+
+
+    def evaluate(self):
+        State.next_state_enum = States_enum.STORED_OPERAND
+        State.update_model_with_next_state(self, self)
+        return State.expression.get_first_operand()
