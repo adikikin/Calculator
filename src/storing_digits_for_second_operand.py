5ce076a39b1dcb02ba5b1c453fca230db0f2f230
Adi <adikikin88@gmail.com>
2021-02-04 15:30:23 +0200

    storing digits for second operand implementation

--- src/storing_digits_for_second_operand.py
+++ src/storing_digits_for_second_operand.py
@@ -9,17 +9,50 @@ class Storing_digits_for_second_operand(State):
         State.next_state_enum = None
 
     def add_digit(self, digit):
-        pass
+        return State.update_expression_and_model(self, 
+                                                 State.expression.add_digit_to_second_operand, 
+                                                 States_enum.STORING_DIGITS_FOR_SECOND_OPERAND,
+                                                 digit)
 
 
     def add_operator(self, operator):
-        pass
+        result = self.__try_to_operate_expression(State.expression.add_operator, operator)
+        if result != States_enum.ERROR.name:        
+            State.next_state_enum = States_enum.STORED_OPERATOR
+            State.update_model_with_next_state(self)
+            return operator
+        else:
+            return result
+
 
 
     def evaluate(self):
-        pass
+        result = self.__try_to_operate_expression(State.expression.evaluate, None)
+        if result != States_enum.ERROR.name:
+            State.next_state_enum = States_enum.STORED_OPERAND
+            State.update_model_with_next_state(self)
+        return result
+
+
+    def __try_to_operate_expression(self, expression_func, arg):
+        try:
+            if arg == None:
+                return expression_func()
+            else:
+                return expression_func(arg)
+        except ZeroDivisionError:
+            State.update_expression_and_model(self, 
+                                              State.expression.reset_expression, 
+                                              States_enum.ERROR,
+                                              None)
+            return States_enum.ERROR.name
+
+
 
 
     def restart(self):
-        pass
+        return State.update_expression_and_model(self,
+                                                 State.expression.reset_expression, 
+                                                 States_enum.START,
+                                                 None)
  
