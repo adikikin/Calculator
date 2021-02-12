3e509e2b51d72ce444c51721d3b8450fa2ece8cf
Adi <adikikin88@gmail.com>
2021-02-12 23:13:40 +0200

    exp_tree implementation + bug fix in add child

--- src/expression_tree.py
+++ src/expression_tree.py
@@ -1,13 +1,21 @@
 from abc import ABC, abstractmethod
-from buttons_enum import Buttons_Enum as Btns
+from src.buttons_enum import Buttons_Enum as Btns
 
 class Component(ABC):
     @abstractmethod
     def __init__(self, item):
         self.value = item
-    
+
+      
     @abstractmethod
     def operate(self):
+        pass
+
+    def get_value(self):
+        return self.value
+
+    @abstractmethod
+    def print_component(self):
         pass
 
 
@@ -18,6 +26,9 @@ class Operand(Component):
     def operate(self):
         return self.value
     
+    def print_component(self):
+        print(self.value)
+    
 
 class Operator(Component):
     @abstractmethod
@@ -25,14 +36,18 @@ class Operator(Component):
         Component.__init__(self,item)
         self.left = None
         self.right = None
-  
 
     def add_child(self, component):
-        if self.left == None:
-            self.left = component
-        else:
-            assert self.right == None
+        if self.right == None:
             self.right = component
+        else:
+            assert self.left == None
+            self.left = component
+
+    def print_component(self):
+        self.left.print_component()
+        self.right.print_component()
+        print(self.value)
 
 
 class Add(Operator):
@@ -86,4 +101,22 @@ class Component_Factory:
         else:
             assert type(item) == int or type(item) == float
             return Operand(item)
+    
+
+class Expression_tree:
+    def __init__(self, postfix_component_list):
+        self.tree = []
+        for i in postfix_component_list:
+            if isinstance(i, Operator):
+                assert len(self.tree) > 1
+                i.add_child(self.tree.pop())
+                i.add_child(self.tree.pop())
+            self.tree.append(i)
+
+    def evaluate(self):
+        assert len(self.tree) == 1
+        return self.tree[0].operate()
+
+    def print_postorder(self):
+        self.tree[0].print_component()
     
