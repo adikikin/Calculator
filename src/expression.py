56d4ac14e47f19aef8f5a1d0fef1a430c1b22350
Adi <adikikin88@gmail.com>
2021-02-13 22:02:33 +0200

    new implementation

--- src/expression.py
+++ src/expression.py
@@ -1,26 +1,23 @@
 import operator 
 from src.buttons_enum import Buttons_Enum as Btns
+from src.infix_to_postfix_converter import Infix_to_postfix_converter
+from src.expression_tree import Expression_tree, Component_Factory
 
 #in the future: expression tree
 class Expression:
     def __init__(self):
-        self.first_operand = 0    
-        self.second_operand = 0
-        self.operator = None
+        self.converter = Infix_to_postfix_converter()
+        self.tree = Expression_tree()
+        self.operand = 0    
         self.sign = 1
-        self.ops = {
-            Btns.ADD.value : operator.add,
-            Btns.SUB.value : operator.sub,
-            Btns.MUL.value : operator.mul,
-            Btns.DIV.value : operator.truediv,
-            }
-    
-    def reset_expression(self):
-        self.first_operand = 0  
-        self.second_operand = 0
-        self.operator = None
-        self.sign = 1
+        self.infix_exp = []
+        self.factory = Component_Factory()
 
+    def reset_expression(self):
+        self.operand = 0  
+        self.sign = 1
+        self.tree.clear_tree()
+        self.infix_exp.clear() 
     
     def flip_sign(self):
         self.sign *= -1
@@ -33,28 +30,28 @@ class Expression:
             operand = operand * 10 + digit
         return operand
 
-    def add_digit_to_first_operand(self, digit):
-        self.first_operand = self.__add_digit(self.first_operand, digit)
+    def add_digit_to_operand(self, digit):
+        self.operand = self.__add_digit(self.operand, digit)
 
-    def add_digit_to_second_operand(self, digit):
-        self.second_operand = self.__add_digit(self.second_operand, digit)
 
-    def get_first_operand(self):
-        return self.first_operand * self.sign
+    def getoperand(self):
+        return self.operand * self.sign
 
     #may throw ZeroDivisionError exception
     def add_operator(self, operator):
-        assert operator in self.ops
-        if self.second_operand != 0:
-            assert self.operator != None
-            self.evaluate()
-        self.operator = operator
-        
+        assert operator in [i.value for i in Btns]
+        assert self.operand != None
+        self.infix_exp.append(self.operand)
+        self.infix_exp.append(operator)
+        self.operand = 0
+
 
     #may throw ZeroDivisionError exception
     def evaluate(self):
-        assert self.operator in self.ops
-        self.first_operand = self.sign * self.ops[self.operator](self.first_operand, self.second_operand)
+        self.infix_exp.append(self.operand)        
+        postfix = self.converter.convert(self.infix_exp)
+        postfix_components = [self.factory.Create_component(i) for i in postfix]
+        self.tree.build_tree(postfix_components)
+        self.operand = self.sign * self.tree.evaluate()
         self.sign = 1
-        self.second_operand = 0
-        return self.first_operand
+        return self.operand
