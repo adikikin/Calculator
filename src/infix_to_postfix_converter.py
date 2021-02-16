f52e9635e45d206491d57304dc66aa89218b7011
Adi <adikikin88@gmail.com>
2021-02-16 13:33:25 +0200

    replaced exception with assert in convert()

--- src/infix_to_postfix_converter.py
+++ src/infix_to_postfix_converter.py
@@ -1,8 +1,6 @@
 from src.common import Buttons_Enum as btn
 from collections import deque
 
-class TooShortExpression(Exception): pass
-class BadInput(Exception): pass
 
 class Infix_to_postfix_converter:
     def __init__(self):
@@ -27,32 +25,18 @@ class Infix_to_postfix_converter:
         while len(self.ops_stack) > 0 and self.__get_precedency(self.ops_stack[-1]) >= self.__get_precedency(operator):
             self.__move_item_from_ops_stack_to_result_exp(postfix_exp)
     
-    def __check_input_length(self, expression):
-        if len(expression) < 3:
-            raise TooShortExpression
-    
+
     def convert(self, expression):
-        self.__check_input_length(expression)
+        assert len(expression) > 2
         result = []
         for item in expression:
             if item in self.precedency_dict:
                 self.__move_higher_and_equal_precedency_ops_to_result_exp(item, result)
                 self.ops_stack.append(item)
-            elif type(item) == int or type(item) == float:
-                result.append(item)
             else:
-                raise BadInput
+                assert type(item) == int or type(item) == float
+                result.append(item)
         while self.ops_stack:
             self.__move_item_from_ops_stack_to_result_exp(result)
         return result
 
-
-
-
-
-# else:
-#     if len(self.ops_stack) == 0 or self.__get_precedency(item) > self.__get_precedency(self.ops_stack[-1]):
-#         self.ops_stack.append(item)
-#     else:
-#         self.__move_higher_and_equal_precedency_ops_to_result_exp(item)
-#         self.ops_stack.append(item)
